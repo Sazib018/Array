845c065456731216cbfcb0414ccc9c043c81ca12
Sazib <mahiburislamsazib@gmail.com>
2024-06-29 22:07:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//task-1
+
+var fruits =['apple','banana','orange'];
+var item = fruits.indexOf("banana");
+fruits[1] ="mango";
+
+var fruit = fruits.pop();
+var fruit= fruits.push("watermelon");
+console.log(fruits);
