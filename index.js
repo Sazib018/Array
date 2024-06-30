9237c94f7005969f4c034c659f0cc745b2bc6d79
Sazib <mahiburislamsazib@gmail.com>
2024-06-30 14:01:20 +0600

    task-2

--- index.js
+++ index.js
@@ -19,20 +19,26 @@ var johnScore = 40;
 //all friends number
 
 var myGrade;
-if (myScore >= 85) {
+if (myScore >= 80) {
   myGrade = "A";
 }
+else if (myScore >= 60){myGrade ="B"}
+else if (myScore >= 50){myGrade ="C"}
+else if (myScore >= 40){myGrade ="D"}
  else {
   myGrade = "F";
 }
-console.log("mygrade:",myGrade);
+console.log("my grade:",myGrade);
 
 //tom number
 
 var tomGrade;
-if (tomScore >= 66) {
+if (tomScore >= 80) {
   tomGrade = "B";
 }
+else if (tomScore >= 60){tomGrade ="B"}
+else if (tomScore >= 50){tomGrade ="C"}
+else if (tomScore >= 40){tomGrade ="D"}
  else {
   tomGrade = "F";
 }
@@ -41,9 +47,12 @@ console.log("tom grade:", tomGrade);
 //jane number
 
 var janeGrade;
-if (janeScore >= 95) {
+if (janeScore >= 80) {
   janeGrade = "A";
 } 
+else if (janeScore >= 60){janeGrade ="B"}
+else if (janeScore >= 50){janeGrade ="C"}
+else if (janeScore >= 40){janeGrade ="D"}
 else {
   janeGrade = "F";
 }
@@ -52,9 +61,12 @@ console.log("jane grade:", janeGrade);
 //peter number
 
 var peterGrade;
-if (peterScore >= 56) {
+if (peterScore >= 80) {
   peterGrade = "A";
-} 
+}
+else if (peterScore >= 60){peterGrade ="B"}
+else if (peterScore >= 50){peterGrade ="C"}
+else if (peterScore >= 40){peterGrade ="D"} 
  else {
   peterGrade = "F";
 }
@@ -63,9 +75,12 @@ console.log("peter grade:", peterGrade);
 //john number
 
 var johnGrade;
-if (johnScore >= 40) {
+if (johnScore >= 80) {
   johnGrade = "D";
 } 
+else if (johnScore >= 60){johnGrade ="B"}
+else if (johnScore >= 50){johnGrade ="C"}
+else if (johnScore >= 40){johnGrade ="D"}
  else {
   johnGrade = "F";
 }
